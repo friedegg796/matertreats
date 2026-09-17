e6eea10f1613cc673919b9d9756a77b6d017cc2d
Hasnain Alahi <hasnainalahiwork@gmail.com>
2026-09-17 10:15:34 +0000

    Thu, Sep 17, 2026, 4:15 PM +06:00

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+
+        </div>
+        <div>
+          <h1>Matertreats</h1>
+          <p>
+            Heavy WIP
+          </p>
+        </div>
+
+      </section>
+
+
+
+      <section id="next-steps">
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/friedegg796" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/awanderingmuslim/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
